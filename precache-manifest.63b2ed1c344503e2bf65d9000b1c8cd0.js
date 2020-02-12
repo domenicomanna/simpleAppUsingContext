d6816e6a26cf4efab4e6cfe164e926c38ae6aea5
Domenico Manna <mannadomenico2849@gmail.com>
2020-02-11 20:45:05 -0500

    Updates

---
+++ precache-manifest.63b2ed1c344503e2bf65d9000b1c8cd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc5b0f2b583df34ca6a2cabd1c0e2a4",
+    "url": "%7Bdomenicomanna%7D.github.io/%7BsimpleAppUsingContext%7D/index.html"
+  },
+  {
+    "revision": "bf5ffd31b786c692aaff",
+    "url": "%7Bdomenicomanna%7D.github.io/%7BsimpleAppUsingContext%7D/static/css/main.20954ede.chunk.css"
+  },
+  {
+    "revision": "3d6e91939638cc35dc9a",
+    "url": "%7Bdomenicomanna%7D.github.io/%7BsimpleAppUsingContext%7D/static/js/2.3677af24.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "%7Bdomenicomanna%7D.github.io/%7BsimpleAppUsingContext%7D/static/js/2.3677af24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf5ffd31b786c692aaff",
+    "url": "%7Bdomenicomanna%7D.github.io/%7BsimpleAppUsingContext%7D/static/js/main.f525f3fc.chunk.js"
+  },
+  {
+    "revision": "3d6086405f8ef73abec9",
+    "url": "%7Bdomenicomanna%7D.github.io/%7BsimpleAppUsingContext%7D/static/js/runtime-main.8b94c182.js"
+  }
+]);
